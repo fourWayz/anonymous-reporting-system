f0ced3aa6a6beae014b9beca85de5c6e74aafb5d
fourWayz <157867069+fourWayz@users.noreply.github.com>
2024-08-19 14:21:27 +0000

    added more complex testing

--- whisperchain/test/whisperChain.test.ts
+++ whisperchain/test/whisperChain.test.ts
@@ -36,11 +36,58 @@ describe("AnonGuard Contract", function () {
 
           // Check that the report exists
         const report = await (whisperchain.connect(owner) as Contract).getReport(reportId);
-        
+
         expect(report.content).to.equal(reportContent);
         expect(report.tag).to.equal(reportTag);
-        expect(report.status).to.equal(0); // Pending
-        expect(report.voteCount).to.equal(0);
+        expect(report.status.toString()).to.equal('0'); // Pending
+        expect(report.voteCount.toString()).to.equal('0');
         expect(report.comments).to.be.an("array").that.is.empty;
     });
+
+    it("should allow an admin to update report status", async function () {
+        const reportContent = "This is an anonymous report.";
+        const reportTag = "Fraud";
+    
+        // Submit a report
+        const tx = await (whisperchain.connect(address2) as Contract).submitReport(reportContent, reportTag);
+
+        // Wait for the transaction to be mined
+        const receipt = await tx.wait(); 
+
+        const log = receipt.logs.find(log => log.fragment?.name === "ReportSubmitted");
+
+        const reportId = log.args[0];
+
+        // Update the report status
+        await (whisperchain.connect(deployer) as Contract).updateReportStatus(reportId, 1); // Reviewed
+    
+        // Check the updated report status
+        const updatedReport = await  (whisperchain.connect(address2) as Contract).getReport(reportId);
+        // console.log(updatedReport)
+        expect(updatedReport.status.toString()).to.equal('1'); // Reviewed
+      });
+
+      it("should allow reviewers to add comments", async function () {
+        const reportContent = "This is an anonymous report.";
+        const reportTag = "Violation";
+    
+          // Submit a report
+          const tx = await (whisperchain.connect(address2) as Contract).submitReport(reportContent, reportTag);
+
+          // Wait for the transaction to be mined
+          const receipt = await tx.wait(); 
+  
+          const log = receipt.logs.find(log => log.fragment?.name === "ReportSubmitted");
+  
+          const reportId = log.args[0];
+
+            // Assign the reviewer role to addr2
+              await (whisperchain.connect(deployer) as Contract).assignRole(address2.address, 1); // Reviewer
+            // Add a comment as a reviewer
+            await (whisperchain.connect(address2) as Contract).addComment(reportId, "This needs further investigation.");
+        
+            // Check the comments
+            const report =  await (whisperchain.connect(deployer) as Contract).getReport(reportId);
+            expect(report.comments[0]).to.equal("This needs further investigation.");
+      });
 })
